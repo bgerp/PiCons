e656562dbc3c19961ee992c7673594f90103b4d4
mitko <mitko@experta.bg>
2019-03-19 14:06:12 +0200

    По голям шрифт

--- StartPageWebroot/index.php
+++ StartPageWebroot/index.php
@@ -5,7 +5,7 @@ if (!empty($_GET['err'])) {
 echo ("
 <form name='bgERP' method=get action='redirect.php'>
 " . (empty($err)?'':"<div><font style='color: red;'>" . $err . "</font></div>") .  
-	"<span>Въведете баркода от работната карта: </span><input name=redirectUrl type=text autofocus></input>
+	"<span style='1color: green; font-size: 12pt'>Въведете баркода от работната карта: </span><input name=redirectUrl type=text autofocus></input>
 </form>
 
 ");
