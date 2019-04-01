46c6fb0c2a9d45989f7a578956a6e28f3a68a6b2
unknown <orlin369@gmail.com>
2019-04-01 14:22:09 +0300

    Changed setup procedure

--- picons_setup.sh
+++ picons_setup.sh
@@ -10,23 +10,15 @@ fi
 
 apt-get -y update
 apt-get -y upgrade
-apt-get install build-essential python-dev python-smbus git php-cli rng-tools
-cd /home/pi/Downloads/
-mkdir libs
-cd libs
-git clone https://github.com/adafruit/Adafruit_Python_MCP3008.git
-cd Adafruit_Python_MCP3008/
-python setup.py install
+apt-get install build-essential python-dev python-smbus git php-cli rng-tools chromium-browser -y
+pip install mcp3008
 pip install dicttoxml
-cd /home/pi
-chmod 0777 /home/pi/PiCons/
-wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add –
-echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list
-apt-get -y update
-apt-get -y install chromium-browser
-cd /home/pi/.config/
+cd ~
+chmod -R 0777 ~/PiCons
+chown -R $USER:$USER ~/PiCons
+cd ~/.config/
 mkdir autostart
-cp /home/pi/PiCons/autoChromium.desktop /home/pi/.config/autostart/autoChromium.desktop
+cp ~/PiCons/autoChromium.desktop ~/.config/autostart/autoChromium.desktop
 echo "@reboot php -S localhost:8181 -t /home/pi/PiCons/StartPageWebroot" > cron.res
 echo "@reboot bash /home/pi/PiCons/picons_autostart.sh" >> cron.res
 crontab cron.res
