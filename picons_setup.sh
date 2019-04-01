2744fb2c7dd18ad4f459301be901c8de43d929e8
unknown <orlin369@gmail.com>
2019-04-01 17:41:06 +0300

    Migration from tilda to absolute path

--- picons_setup.sh
+++ picons_setup.sh
@@ -13,12 +13,12 @@ apt-get -y upgrade
 apt-get install build-essential python-dev python-smbus git php-cli rng-tools chromium-browser -y
 pip install mcp3008
 pip install dicttoxml
-cd ~
-chmod -R 0777 ~/PiCons
-chown -R $USER:$USER ~/PiCons
-cd ~/.config/
+cd /home/pi
+chmod -R 0777 /home/pi/PiCons
+chown -R $USER:$USER /home/pi/PiCons
+cd /home/pi/.config/
 mkdir autostart
-cp ~/PiCons/autoChromium.desktop ~/.config/autostart/autoChromium.desktop
+cp /home/pi/PiCons/autoChromium.desktop /home/pi/.config/autostart/autoChromium.desktop
 echo "@reboot php -S localhost:8181 -t /home/pi/PiCons/StartPageWebroot" > cron.res
 echo "@reboot bash /home/pi/PiCons/picons_autostart.sh" >> cron.res
 crontab cron.res
