abc83ba6f3be6f38e046d25047f4d0c41798bbb5
Dimitar Minekov <mitko@extrapack.com>
2019-04-08 16:16:22 +0300

    check URL - case-insensitive

--- StartPageWebroot/redirect.php
+++ StartPageWebroot/redirect.php
@@ -1,5 +1,5 @@
 <?php 
-if (strpos($_GET['redirectUrl'], 'https://bags.bg') !== false ) {
+if (stripos($_GET['redirectUrl'], 'https://bags.bg') !== false ) {
     header("Location: " . $_GET['redirectUrl']);
 } else {
     header("Location: http://localhost:8181/?err=Грешен баркод!");
