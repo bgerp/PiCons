14973384d516f8e3375380c78ad09acafd29e72d
Dimitar Minekov <mitko@experta.bg>
2019-08-14 17:54:27 +0300

    php web server by default

--- picons_setup.sh
+++ picons_setup.sh
@@ -18,9 +18,9 @@ chmod -R +x /home/pi/PiCons
 cd /home/pi/.config/
 mkdir autostart
 cp /home/pi/PiCons/autoChromium.desktop /home/pi/.config/autostart/autoChromium.desktop
-echo "@reboot php -S localhost:8181 -t /home/pi/PiCons/StartPageWebroot" > cron.res
-echo "@reboot bash /home/pi/PiCons/picons_autostart.sh" >> cron.res
-echo "* * * * * php /home/pi/PiCons/watchDog.php" >> cron.res
+echo "@reboot php -S localhost:80 -t /home/pi/PiCons/ 2>&1 &" > cron.res
+#echo "@reboot bash /home/pi/PiCons/picons_autostart.sh" >> cron.res
+#echo "* * * * * php /home/pi/PiCons/watchDog.php" >> cron.res
 crontab cron.res
 rm cron.res
 echo "You should restart the system."
