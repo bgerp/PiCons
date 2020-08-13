7e6773e9f0770d706daf23f8571e4fdefd5794ef
Orlin Dimitrov <orlin369@gmail.com>
2020-08-13 16:16:13 +0300

    Move device to data

--- PiCons/data/device.py
+++ PiCons/data/device.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
+"""
 
 MIT License
 
-Copyright (c) [2016] [POLYGON Team Ltd.]
+Copyright (c) [2020] [POLYGON Team Ltd.]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,18 +25,16 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-'''
+"""
 
-'''http://user:pass@ip.address/'''
-
-# http://ipaddress/?RelayOutputs=all&DigitalInputs=all&CounterInputs=all&AnalogInputs=all&ElectronicScales=all
+#region File Attributes
 
 ## Author of the file.
 __author__ = "Orlin Dimitrov"
 
 ## Copyrighter
 #  @see http://polygonteam.com/
-__copyright__ = "Copyright 2016, POLYGON Team Ltd."
+__copyright__ = "Copyright 2020, POLYGON Team Ltd."
 
 ## Credits
 __credits__ = ["Angel Boyarov"]
@@ -58,30 +56,88 @@ __email__ = "or.dimitrov@polygonteam.com"
 ## File status.
 __status__ = "Debug"
 
-class Device():
-    __key = None
+#endregion
+
+class Device:
+    """Device"""
+
+    #region Attributes
+
     __name = None
+    """Name"""
+
     __unit = None
+    """Units"""
+
     __id = None
-    
+    """Identification"""
+
+    __values = None
+    """Values"""
+
+    #endregion
+
+    #region Propertyes
+
+    @property
+    def name(self):
+        """Name"""
+
+        return self.__name
+
+    @property
+    def unit(self):
+        """Units"""
+
+        return self.__unit
+
+    @property
+    def id(self):
+        """Identification"""
+
+        return self.__id
+
+    @property
+    def values(self):
+        """Values"""
+
+        return self.__values
+
+    @property
+    def is_valid(self):
+        """Is valid property."""
+
+        return self.name is not None and self.unit is not None and self.id is not None
+
+    @update_values.setter
+    def update_values(self, values):
+        self.__values = values
+
+    #endregion
+
+    #region constructor
+
     def __init__(self, name, unit, id):
+        """Constructor
+
+        Parameters
+        ----------
+        name : str
+            Name of the device.
+        unit : int
+            Device unit.
+        id : int
+            ID
+        """
+
         self.__name = name
         self.__unit = unit
         self.__id = id
         self.__values = []
-        
-    def getName(self):
-        return self.__name
-        
-    def getUnit(self):
-        return self.__unit
-        
-    def getID(self):
-        return self.__id
-        
-    def update_values(self, values):
-        self.__values = values
-        print self.__values
-        
-    def isValid(self):
-        return (self.__key != None and self.__Name and self.__unit != None and self.__id != None)
+
+    def __str__(self):
+        return "Device; Name: {}; Unit: {}; ID: {}; Values: {}".format(self.name, self.unit, self.id, self.values)
+
+    __repr__ = __str__
+
+    #endregion
