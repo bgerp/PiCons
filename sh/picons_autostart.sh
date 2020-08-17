49fbf5954ad058c411e4667396cfeadc3d5e2538
Orlin Dimitrov <orlin369@gmail.com>
2020-08-17 13:01:40 +0300

    Modified /sh folder

--- sh/picons_autostart.sh
+++
@@ -1,9 +0,0 @@
-
-# Start the PiCons server.
-ps cax | grep python > /dev/null
-if [ $? -eq 0 ]; then
-  echo "The PiCons server is runing."
-else
-  echo "Starting the PiCons server."
-  sudo python /home/pi/PiCons/main.py > /dev/null 2>&1 &
-fi
--- sh/picons_setup.sh
+++ sh/picons_setup.sh
@@ -1,29 +1,23 @@
 ﻿#!/bin/bash
-# Pi-Cons Setup procedures.
 
-usr=$(env | grep SUDO_USER | cut -d= -f 2)
+apt-get update -y
+apt-get upgrade -y
 
-if [ -z $usr ] && [ $USER = "root" ]
-then
-    echo "The script needs to run as root" && exit 1
-fi
-
-apt-get -y update
-apt-get -y upgrade
 apt-get install build-essential python-dev python-smbus git php-cli rng-tools chromium-browser -y
-pip install mcp3008
+
 pip install dicttoxml
+
 cd /home/pi
 chmod -R +x /home/pi/PiCons
+
 cd /home/pi/.config/
 mkdir autostart
 cp /home/pi/PiCons/autoChromium.desktop /home/pi/.config/autostart/autoChromium.desktop
+
 echo "@reboot php -S localhost:80 -t /home/pi/PiCons/ 2>&1 &" > cron.res
 echo "@reboot php -S localhost:8181 -t /home/pi/PiCons/StartPageWebroot/ 2>&1 &" >> cron.res
 #echo "@reboot bash /home/pi/PiCons/picons_autostart.sh" >> cron.res
 #echo "* * * * * php /home/pi/PiCons/watchDog.php" >> cron.res
 crontab cron.res
 rm cron.res
-echo "You should restart the system."
-echo "Type: sudo reboot"
 reboot
