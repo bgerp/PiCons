3f20ec016050a51649a11c76235579c3620b19fc
Orlin Dimitrov <orlin369@gmail.com>
2020-08-19 16:36:42 +0300

    Extend PiCons test

--- sh/picons_test.sh
+++ sh/picons_test.sh
@@ -1,30 +1,73 @@
 #!/bin/bash
-# My first script
+
+# wget -q -O - "http://admin:admin@127.0.1.1:8080/?RelayOutputs=all&DigitalInputs=all&CounterInputs=all&AnalogInputs=all&ElectronicScales=all"
+# wget -q -O - "http://127.0.1.1:8080/?RelayOutputs=all&DigitalInputs=all&CounterInputs=all&AnalogInputs=all&ElectronicScales=all"
+# wget -q -O - "http://127.0.1.1:8080?"
 
 echo "Begin ..."
-wget -qO- http://127.0.0.1/?Relay1=1
 echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay1=1"
 sleep 1
-wget -qO- http://127.0.0.1/?Relay1=0
 echo ""
-sleep 1
-wget -qO- http://127.0.0.1/?Relay2=1
 echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay1=0"
 sleep 1
-wget -qO- http://127.0.0.1/?Relay2=0
 echo ""
-sleep 1
-wget -qO- http://127.0.0.1/?Relay3=1
 echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay2=1"
 sleep 1
-wget -qO- http://127.0.0.1/?Relay3=0
 echo ""
-sleep 1
-wget -qO- http://127.0.0.1/?Relay4=1
 echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay2=0"
 sleep 1
-wget -qO- http://127.0.0.1/?Relay4=0
 echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay3=1"
 sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay3=0"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay4=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080/?Relay4=0"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080?RelayOutputs=all"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080?Relay1=1&Relay2=1&Relay3=1&Relay4=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080?Relay1=0&Relay2=0&Relay3=0&Relay4=0"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080?DigitalInputs=all"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080?CounterInputs=all"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080?AnalogInputs=all"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080?ElectronicScales=all"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8080/?RelayOutputs=all&DigitalInputs=all&CounterInputs=all&AnalogInputs=all&ElectronicScales=all"
+sleep 1
+echo ""
 echo ""
 echo "Done ..."
