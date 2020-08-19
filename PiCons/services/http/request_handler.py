6c952687d6a6aa47eafe5208a8d762b8f45a449c
Orlin Dimitrov <orlin369@gmail.com>
2020-08-19 16:34:18 +0300

    Code optimization

--- PiCons/services/http/request_handler.py
+++ PiCons/services/http/request_handler.py
@@ -27,9 +27,7 @@ SOFTWARE.
 
 """
 
-import json
 from http.server import BaseHTTPRequestHandler
-from urllib import parse
 
 from utils.logger import get_logger
 from utils.settings import AppSettings
@@ -49,73 +47,6 @@ class RequestHandler(BaseHTTPRequestHandler):
     """API path to EVOK handler."""
 
     __logger = get_logger(__name__)
-
-#endregion
-
-#region Public Methods
-
-    def do_POST(self):
-        """Handler for the POST requests."""
-
-        parsed_path = parse.urlparse(self.path)
-
-        if parsed_path.path == self.__api_path_evok:
-            self.__api_evok_webhooks_handler()
-
-        else:
-            # The response.
-            self.send_response(200)
-            self.send_header("Content-Type",
-                            "text/plain; charset=utf-8")
-            self.end_headers()
-            self.wfile.write("OK".encode("utf-8"))
-
-    def do_GET(self):
-        """Handler for the GET requests."""
-
-        # Get the key.
-        key = self._settings.get_credentials_as_b64
-
-        # Check if it home IP or authorized client respons it.
-        if (self.headers["Authorization"] == "Basic " + key) or (self.client_address[0] == "127.0.0.1"):
-            # Just pass and proseed.
-            pass
-
-        # Else redirect to authorize.
-        elif self.headers["Authorization"] == None:
-            self.do_AUTHHEAD()
-            self.wfile.write("no auth header received")
-            pass
-
-        # Else redirect to authorize.
-        else:
-            self.do_AUTHHEAD()
-            self.wfile.write(self.headers["Authorization"])
-            self.wfile.write("not authenticated")
-            pass
-
-
-        mime_type = self._get_mime()
-        page = self._do_page()
-
-        self.send_response(200)
-        self.send_header("Content-type", mime_type)
-        self.send_header("Access-Control-Allow-Origin", "*") # This may be usefull as a settings. Only the central server can call the IO device.
-        #self.send_header("Access-Control-Allow-Methods", "GET") # This may be usefull only with get method to access the device.
-        self.end_headers()
-        # Send the page
-        # self.wfile.write("OK".encode("utf-8"))
-        self.wfile.write(page)
-
-    def do_AUTHHEAD(self):
-        """Handler for the authorization."""
-
-        # Set MIME.
-        mime_type = self._get_mime()
-        self.send_response(401)
-        self.send_header("WWW-Authenticate", "Basic realm=\"Test\"")
-        self.send_header("Content-type", mime_type)
-        self.end_headers()
 
 #endregion
 
@@ -140,49 +71,118 @@ class RequestHandler(BaseHTTPRequestHandler):
         mime_type = "text/html"
         split_count = len(split_string)
 
-        if(split_count >= 2):
+        if split_count >= 2:
 
             extention = split_string[split_count - 1]
 
-            if(extention == "css"):
+            if extention == "css":
                 mime_type = "text/css"
 
-            elif(extention == "jpg"):
+            elif extention == "jpg":
                 mime_type = "image/jpeg"
 
-            elif(extention == "jpeg"):
+            elif extention == "jpeg":
                 mime_type = "image/jpeg"
 
-            elif(extention == "png"):
+            elif extention == "png":
                 mime_type = "image/png"
 
-            elif(extention == "bmp"):
+            elif extention == "bmp":
                 mime_type = "image/bmp"
 
-            elif(extention == "gif"):
+            elif extention == "gif":
                 mime_type = "image/gif"
 
-            elif(extention == "emf"):
+            elif extention == "emf":
                 mime_type = "image/emf"
 
-            elif(extention == "ico"):
+            elif extention == "ico":
                 mime_type = "image/ico"
 
-            elif(extention == "csv"):
+            elif extention == "csv":
                 mime_type = "text/csv"
 
-            elif(extention == "html"):
+            elif extention == "html":
                 mime_type = "text/html"
 
-            elif(extention == "js"):
+            elif extention == "js":
                 mime_type = "text/javascript"
 
-            elif(extention == "xml"):
+            elif extention == "xml":
                 mime_type = "text/xml"
 
             else:
                 mime_type = "text/html"
 
         return mime_type
+
+#endregion
+
+#region Public Methods
+
+    def do_POST(self):
+        """Handler for the POST requests."""
+
+        # The response.
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write("OK".encode("utf-8"))
+
+    def do_GET(self):
+        """Handler for the GET requests."""
+
+        # Get the key.
+        key = self._settings.get_credentials_as_b64
+
+        # Check if it home IP or authorized client respons it.
+        if (self.headers["Authorization"] == "Basic " + key)\
+             or (self.client_address[0] == "127.0.0.1"):
+
+            # Just pass and proseed.
+            pass
+
+        # Else redirect to authorize.
+        elif self.headers["Authorization"] is None:
+            self.do_AUTHHEAD()
+            self.wfile.write("no auth header received")
+
+        # Else redirect to authorize.
+        else:
+            self.do_AUTHHEAD()
+            self.wfile.write(self.headers["Authorization"])
+            self.wfile.write("not authenticated")
+
+
+        mime_type = self._get_mime()
+        page = self._do_page()
+
+        # Status code.
+        self.send_response(200)
+
+        # Add content type.
+        self.send_header("Content-type", mime_type)
+
+         # This may be usefull as a settings. Only the central server can call the IO device.
+        self.send_header("Access-Control-Allow-Origin", "*")
+
+         # This may be usefull only with get method to access the device.
+        #self.send_header("Access-Control-Allow-Methods", "GET")
+
+        # End headers.
+        self.end_headers()
+
+        # Send the page
+        self.wfile.write(page)
+
+    def do_AUTHHEAD(self):
+        """Handler for the authorization."""
+
+        # Set MIME.
+        mime_type = self._get_mime()
+        self.send_response(401)
+        self.send_header("WWW-Authenticate", "Basic realm=\"Test\"")
+        self.send_header("Content-type", mime_type)
+        self.end_headers()
 
 #endregion
