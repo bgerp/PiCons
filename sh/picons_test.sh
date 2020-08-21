41e1414b51a7327b6806ada51a0e15a6e2fbec9b
Orlin Dimitrov <orlin369@gmail.com>
2020-08-21 14:39:14 +0300

    Service port change

--- sh/picons_test.sh
+++ sh/picons_test.sh
@@ -4,119 +4,135 @@ echo "Begin ..."
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080/?Relay1=1"
+wget -q -O - "http://127.0.1.1:8090/?Relay1=1"
 sleep 1
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?Relay1=0"
+wget -q -O - "http://127.0.1.1:8090/?Relay1=0"
 sleep 1
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?Relay2=1"
+wget -q -O - "http://127.0.1.1:8090/?Relay2=1"
 sleep 1
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?Relay2=0"
+wget -q -O - "http://127.0.1.1:8090/?Relay2=0"
 sleep 1
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?Relay3=1"
+wget -q -O - "http://127.0.1.1:8090/?Relay3=1"
 sleep 1
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?Relay3=0"
+wget -q -O - "http://127.0.1.1:8090/?Relay3=0"
 sleep 1
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?Relay4=1"
+wget -q -O - "http://127.0.1.1:8090/?Relay4=1"
 sleep 1
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?Relay4=0"
-sleep 1
-echo ""
-echo ""
-
-# =============================================================================
-wget -q -O - "http://127.0.1.1:8080/?ToggleRelay1=1"
-sleep 1
-echo ""
-echo ""
-wget -q -O - "http://127.0.1.1:8080/?ToggleRelay2=1"
-sleep 1
-echo ""
-echo ""
-wget -q -O - "http://127.0.1.1:8080/?ToggleRelay3=1"
-sleep 1
-echo ""
-echo ""
-wget -q -O - "http://127.0.1.1:8080/?ToggleRelay4=1"
+wget -q -O - "http://127.0.1.1:8090/?Relay4=0"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080/?PulseRelay1=2"
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay1=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay2=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay3=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay4=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay1=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay2=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay3=1"
+sleep 1
+echo ""
+echo ""
+wget -q -O - "http://127.0.1.1:8090/?ToggleRelay4=1"
+sleep 1
+echo ""
+echo ""
+
+# =============================================================================
+wget -q -O - "http://127.0.1.1:8090/?PulseRelay1=2"
 sleep 5
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?PulseRelay2=2"
+wget -q -O - "http://127.0.1.1:8090/?PulseRelay2=2"
 sleep 5
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?PulseRelay3=2"
+wget -q -O - "http://127.0.1.1:8090/?PulseRelay3=2"
 sleep 5
 echo ""
 echo ""
-wget -q -O - "http://127.0.1.1:8080/?PulseRelay4=2"
+wget -q -O - "http://127.0.1.1:8090/?PulseRelay4=2"
 sleep 5
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080?RelayOutputs=all"
+wget -q -O - "http://127.0.1.1:8090?RelayOutputs=all"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080?Relay1=1&Relay2=1&Relay3=1&Relay4=1"
+wget -q -O - "http://127.0.1.1:8090?Relay1=1&Relay2=1&Relay3=1&Relay4=1"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080?Relay1=0&Relay2=0&Relay3=0&Relay4=0"
+wget -q -O - "http://127.0.1.1:8090?Relay1=0&Relay2=0&Relay3=0&Relay4=0"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080?DigitalInputs=all"
+wget -q -O - "http://127.0.1.1:8090?DigitalInputs=all"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080?CounterInputs=all"
+wget -q -O - "http://127.0.1.1:8090?CounterInputs=all"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080?AnalogInputs=all"
+wget -q -O - "http://127.0.1.1:8090?AnalogInputs=all"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080?ElectronicScales=all"
+wget -q -O - "http://127.0.1.1:8090?ElectronicScales=all"
 sleep 1
 echo ""
 echo ""
 
 # =============================================================================
-wget -q -O - "http://127.0.1.1:8080/?RelayOutputs=all&DigitalInputs=all&CounterInputs=all&AnalogInputs=all&ElectronicScales=all"
+wget -q -O - "http://127.0.1.1:8090/?RelayOutputs=all&DigitalInputs=all&CounterInputs=all&AnalogInputs=all&ElectronicScales=all"
 sleep 1
 echo ""
 echo ""
