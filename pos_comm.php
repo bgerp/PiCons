5ec9f1a2521291d3534ba03c70eb5d161ec9628d
mitko <mitko@experta.bg>
2023-11-14 17:13:27 +0200

    save commit

--- pos_comm.php
+++ pos_comm.php
@@ -1,5 +1,5 @@
 <?php
-exec ('powershell C:\Users\1064\PiCons\comport_pos.ps1');
+// exec ('powershell C:\Users\epuser\Documents\GitHub\PiCons\pos_comport.ps1');
 
 define ('DEVICE', 'COM1');
 clearstatcache();
@@ -15,13 +15,68 @@ foreach ($rApproval as $b) {
 }
 echo "writed ...\n\r";
 $end = false;
-$res ="";
-while ($end != true) {
-   $res .= ord(fgetc($fp)) . " ";
-   if (strlen($res) == 7) {
+$res = [];
+do {
+   $res[] = ord(fgetc($fp));
+   if (end($res) == 3) {
 	   $end = true;
+	   // Четем кода по четност
+	   $resPCheck = ord(fgetc($fp));
    }
-}
-echo ($res);
 
-// Търсим първият стринг с дължина 6 и завършващ на 'B'
+} while ($end != true);
+
+//fclose($fp);
+// Махаме първия байт 06 - ACK - Acknowledge
+unset($res[0]);
+// Махаме втория байт 02 - STX - Start of Text
+unset($res[1]);
+
+// Проверяваме сумата по четност
+$PCheck = 0;
+foreach ($res as $byte) {
+	$PCheck ^= $byte; 
+}
+if ($PCheck != $resPCheck) die ("Parity Check ERROR!");
+
+var_dump ($res, $PCheck, $resPCheck);
+
+
+//------------ Пращаме сума
+
+     // $cmd = "06    02    34    30    31    31    30 30 30 30 30 30 30 30 30 31 35 38 39 37 35 39 39 39 39 03 30"
+
+$rSum = array(0x06,0x02,0x34,0x30,0x31,0x31,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x31,0x35,0x38,0x39,0x37,0x35,0x39,0x39,0x39,0x39,0x03,0x30);
+
+foreach ($rSum as $b) {
+	if (fputs($fp, chr($b)) === false) {
+		die("Can`t write to port.");
+	}
+}
+echo "writed SUM...\n\r";
+$end = false;
+$res = [];
+do {
+   $res[] = ord(fgetc($fp));
+   if (end($res) == 3) {
+	   $end = true;
+	   // Четем кода по четност
+	   $resPCheck = ord(fgetc($fp));
+   }
+
+} while ($end != true);
+
+fclose($fp);
+// Махаме първия байт 06 - ACK - Acknowledge
+unset($res[0]);
+// Махаме втория байт 02 - STX - Start of Text
+unset($res[1]);
+
+// Проверяваме сумата по четност
+$PCheck = 0;
+foreach ($res as $byte) {
+	$PCheck ^= $byte; 
+}
+if ($PCheck != $resPCheck) die ("Parity Check ERROR!");
+
+var_dump ($res, $PCheck, $resPCheck);
